8136fea508db0ca5d829543221201071fe7e684f
Bryan MacFarlane <bryanmac@microsoft.com>
2014-09-29 23:13:44 -0400

    shellscript task uses filepath

---
+++ src/agent/tasks/ShellScript/1.0.1/shellscript.js
@@ -0,0 +1,39 @@
+// 
+// Copyright (c) Microsoft and contributors.  All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// 
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+
+var path = require('path');
+var shell = require('shelljs/global');
+
+exports.execute = function(ctx, callback) {
+	var _buffer;
+
+	var scriptPath = [ctx.inputs.scriptPath];
+
+	// cwd is optional - we use folder of script as working directory if not set.
+	var cwd = ctx.inputs.cwd;
+	if (!cwd || cwd.length == 0) {
+		cwd = path.dirname(scriptPath);
+	}
+	ctx.verbose('cwd: ' + cwd);
+
+	cd(cwd);
+
+	// shell script runner
+	
+	ctx.util.spawn('sh', scriptPath, { cwd: cwd }, callback);
+}
+
+// TODO: (bryanmac) system needs to chmod on the script? is that configurable?
