315c76a0726d32931e519690dafe12f2acff2d42
administrator <administrator@opens-mbp.guest.corp.microsoft.com>
2016-03-02 20:51:51 +0530

    add support for uploading raw files

--- src/agent/codecoveragepublisher.ts
+++ src/agent/codecoveragepublisher.ts
@@ -26,74 +26,7 @@ export class CodeCoveragePublisher {
 
     private buildId: number;
 
-    private project: string;    
-
-    //-----------------------------------------------------
-    // Read code coverage results from summary file.
-    // - codeCoverageSummaryFile: string () - location of the code coverage summary file 
-    //-----------------------------------------------------    
-    private readCodeCoverageSummary(codeCoverageSummaryFile: string): Q.Promise<testifm.CodeCoverageData> {
-        var defer = Q.defer<testifm.CodeCoverageData>();
-        var _this = this;
-        if(codeCoverageSummaryFile){
-            _this.codeCoverageReader.getCodeCoverageSummary(codeCoverageSummaryFile).then(function(codeCoverageStatistics) {
-                defer.resolve(codeCoverageStatistics);
-            }).fail(function(err) {
-                defer.reject(err);
-            }); 
-        }
-        else{
-            defer.resolve(null);
-        }
-
-        return defer.promise;
-    } 
-    
-    //-----------------------------------------------------
-    // publish code coverage files to server
-    // - reportDirectory: code coverage report directory
-    // - additionalCodeCoverageFiles: additional code coverage files
-    //-----------------------------------------------------
-    public publishCodeCoverageFiles(): Q.Promise<any> {
-      var defer = Q.defer();  
-      var containerId = parseInt(this.executionContext.variables[ctxm.WellKnownVariables.containerId]);
-      var summaryFile = this.command.properties["summaryfile"];
-      var reportDirectory = this.command.properties["reportdirectory"]; 
-      var additionalCodeCoverageFiles = this.command.properties["additionalcodecoveragefiles"];
-      
-      if(!reportDirectory){
-          reportDirectory = path.join(shell.tempdir(), "CodeCoverageReport_" + this.buildId);
-          shell.mkdir('-p', reportDirectory);
-      }
-      
-      // copy the summary file into report directory
-      shell.cp('-f', summaryFile, reportDirectory);
-      
-      if(reportDirectory){
-        var artifactName = "Code Coverage Report_" + this.buildId; 
-        var ret = fc.copyToFileContainer(this.executionContext, reportDirectory, containerId, "/" + artifactName).then((artifactLocation: string) => {
-            this.command.info('Associating artifact ' + artifactLocation + ' ...');
-		
-			var buildId: number = this.buildId;
-			var artifact: buildifm.BuildArtifact = <buildifm.BuildArtifact>{
-				name: artifactName,
-				resource: {
-					type: "container",
-					data: artifactLocation
-				}
-			};
-			
-			var webapi = this.executionContext.getWebApi();
-			var buildClient = webapi.getQBuildApi();
-			return buildClient.createArtifact(artifact, buildId, this.executionContext.variables[ctxm.WellKnownVariables.projectId]);
-		}).fail(function(err) {
-            defer.reject(err);
-        });  
-      }
-      
-      defer.resolve(ret);
-      return defer.promise;      
-    }
+    private project: string;   
     
     //-----------------------------------------------------
     // Publish code coverage
@@ -115,6 +48,143 @@ export class CodeCoveragePublisher {
 
         return defer.promise;
     }
+    
+    //-----------------------------------------------------
+    // publish code coverage files to server
+    // - reportDirectory: code coverage report directory
+    // - additionalCodeCoverageFiles: additional code coverage files
+    //-----------------------------------------------------
+    public publishCodeCoverageFiles(): Q.Promise<any> {
+      var defer = Q.defer();  
+      var containerId = parseInt(this.executionContext.variables[ctxm.WellKnownVariables.containerId]);
+      var summaryFile = this.command.properties["summaryfile"];
+      var reportDirectory = this.command.properties["reportdirectory"]; 
+      var additionalCodeCoverageFiles = this.command.properties["additionalcodecoveragefiles"];
+      var codeCoverageArtifactName = "Code Coverage Report_" + this.buildId; 
+      var newReportDirectory = reportDirectory;
+      
+      if(!newReportDirectory){
+          newReportDirectory = path.join(shell.tempdir(), "CodeCoverageReport_" + this.buildId);
+          shell.mkdir('-p', newReportDirectory);
+      }
+      
+      // copy the summary file into report directory
+      shell.cp('-f', summaryFile, newReportDirectory);
+      
+      var ret = this.uploadArtifact(reportDirectory, codeCoverageArtifactName, containerId);
+      
+      if(additionalCodeCoverageFiles){
+          this.command.info("here");
+          var rawFilesDirectory = path.join(shell.tempdir(), "CodeCoverageFiles_" + this.buildId);
+          shell.mkdir('-p', rawFilesDirectory);
+          var rawFiles : string[] = additionalCodeCoverageFiles.split(",");
+          this.copyRawFiles(rawFiles, rawFilesDirectory);
+          var rawFilesArtfactName = "Code Coverage Files_" + this.buildId
+          ret = this.uploadArtifact(rawFilesDirectory, rawFilesArtfactName, containerId);
+      }
+      
+      // clean generated directories
+      if(!reportDirectory){
+          shell.rm('-rf', newReportDirectory);
+      }
+      
+      if(additionalCodeCoverageFiles){
+          shell.rm('rf', rawFilesDirectory);
+      }
+      
+      defer.resolve(ret);
+      return defer.promise;      
+    }
+    
+    private copyRawFiles(additionalCodeCoverageFiles : string[], rawFilesDirectory : string){
+        if(additionalCodeCoverageFiles.length >1){
+            additionalCodeCoverageFiles = this.sortStringArray(additionalCodeCoverageFiles); 
+            var numberOfFiles = additionalCodeCoverageFiles.length; 
+            var commonPath = this.sharedSubString(additionalCodeCoverageFiles[0], additionalCodeCoverageFiles[numberOfFiles-1] )  
+        }
+        
+        additionalCodeCoverageFiles.forEach(file => {
+            if(commonPath){
+                var newFile : string = file.replace(commonPath, "");
+            }
+            else{
+                var newFile : string = path.basename(file);
+            }
+            
+            newFile = path.join(rawFilesDirectory, newFile);
+            shell.mkdir('-p', path.dirname(newFile));
+            shell.cp('-f', file, newFile)
+        });
+    }
+    
+    private sharedSubString(string1: string, string2: string): string{
+        var ret  = "";
+        var index = 1;
+        while(string1.substring(0, index) == string2.substring(0, index)){
+            ret = string1.substring(0, index);
+            index++;
+        }
+        return ret;
+    }
+    
+    private sortStringArray(files): string[]{
+        var sortedFiles : string[] = files.sort((a, b) =>{
+            if(a > b){
+                return 1;
+            }
+            else if(a < b){
+                return -1;
+            }
+            else{
+                return 0;
+            }
+        });
+        return sortedFiles;
+    }
+    
+    private uploadArtifact(path :string, artifactName: string, containerId : number) : Q.Promise<any> {
+        var defer = Q.defer(); 
+        return fc.copyToFileContainer(this.executionContext, path, containerId, "/" + artifactName).then((artifactLocation: string) => {
+            this.command.info('Associating artifact ' + artifactLocation + ' ...');
+		
+			var buildId: number = this.buildId;
+			var artifact: buildifm.BuildArtifact = <buildifm.BuildArtifact>{
+				name: artifactName,
+				resource: {
+					type: "container",
+					data: artifactLocation
+				}
+			};
+			
+			var webapi = this.executionContext.getWebApi();
+			var buildClient = webapi.getQBuildApi();
+			return buildClient.createArtifact(artifact, buildId, this.executionContext.variables[ctxm.WellKnownVariables.projectId]);
+		}).fail(function(err) {
+            defer.reject(err);
+        });  
+    }
+    
+    
+    //-----------------------------------------------------
+    // Read code coverage results from summary file.
+    // - codeCoverageSummaryFile: string () - location of the code coverage summary file 
+    //-----------------------------------------------------    
+    private readCodeCoverageSummary(codeCoverageSummaryFile: string): Q.Promise<testifm.CodeCoverageData> {
+        var defer = Q.defer<testifm.CodeCoverageData>();
+        var _this = this;
+        if(codeCoverageSummaryFile){
+            _this.codeCoverageReader.getCodeCoverageSummary(codeCoverageSummaryFile).then(function(codeCoverageStatistics) {
+                defer.resolve(codeCoverageStatistics);
+            }).fail(function(err) {
+                defer.reject(err);
+            }); 
+        }
+        else{
+            defer.resolve(null);
+        }
+
+        return defer.promise;
+    } 
 }
 
 //-----------------------------------------------------
