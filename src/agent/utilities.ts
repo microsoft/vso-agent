5492bb57024cf8a9e203340f4f22afa22ba33afb
Nitin Gurram <nigurr@microsoft.com>
2016-03-23 17:17:58 +0530

    Fixed issue in utilities

--- src/agent/utilities.ts
+++ src/agent/utilities.ts
@@ -51,7 +51,6 @@ export function isDirectoryExists(path: string): boolean {
     catch (error) {
         return false;
     }
-    return true;
 }
 
 // returns true if path exists and it is a file else false.
@@ -62,7 +61,6 @@ export function isFileExists(path: string): boolean {
     catch (error) {
         return false;
     }
-    return true;
 }
 
 // TODO: offer these module level context-less helper functions in utilities below
